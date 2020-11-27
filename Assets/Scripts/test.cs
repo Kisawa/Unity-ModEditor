3b31db66642ed6825eb579735daba3659bf4ec85
SeasarViz <1397774741@qq.com>
2020-11-27 19:28:09 +0800

    update

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 
 public class test : ScriptableObject
 {
+    const string path = "Assets/Scripts/test.asset";
+
     [MenuItem("Tools/Create Test")]
     static void Create() 
     {
         test t = CreateInstance<test>();
-        AssetDatabase.CreateAsset(t, "Assets/Scripts/test.asset");
+        //AssetDatabase.CreateAsset(t, "Assets/Scripts/test.asset");
         //AssetDatabase.ImportAsset("Assets/Scripts/test.asset");
         t.res = true;
-        AssetDatabase.CreateAsset(t, "Assets/Scripts/test.asset");
-        AssetDatabase.ImportAsset("Assets/Scripts/test.asset");
+        t.t1 = CreateInstance<test1>();
+        t.t1.str = "viz";
+        AssetDatabase.AddObjectToAsset(t.t1, path);
+        AssetDatabase.CreateAsset(t, path);
+        AssetDatabase.ImportAsset(path);
     }
 
     //public ExposedReference<Camera> sceneCamera;
     //public ExposedReference<GameObject> sceneGameObject;
 
+    [MenuItem("Tools/Delete Test1")]
+    static void DeleteTest1() 
+    {
+        test t = AssetDatabase.LoadAssetAtPath<test>(path);
+        Object.DestroyImmediate(t.t1, true);
+        t.t1 = null;
+        AssetDatabase.ImportAsset(path);
+    }
+
     public bool res;
+    public test1 t1;
+}
+
+public class test1 : ScriptableObject
+{
+    public string str;
 }
